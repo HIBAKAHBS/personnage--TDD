e8ced58a8bb08d30c7018bd203ecea4b281853e5
hiba kahbs <hibakahbs@gemail.com>
2025-12-14 22:56:43 +0100

    nv

--- main/java/com/hiba/FizzBuzz/FizzBuzz.java
+++ main/java/com/hiba/FizzBuzz/FizzBuzz.java
@@ -1,4 +1,18 @@
 package com.hiba.FizzBuzz;
 
 public class FizzBuzz {
+
+    public static String de(int n) {
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (n % 3 == 0) {
+            return "Fizz";
+        }
+        if (n % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(n);
+    }
 }
+
